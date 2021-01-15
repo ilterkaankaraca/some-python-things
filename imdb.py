0e76fd4da752c8ca928c10a8372d1c54bc4fc0a8
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-01-15 09:57:36 +0100

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -1,1 +1,6 @@
 #to get rating information 
+#https://www.imdb.com/user/ur57221798/ratings/export
+import urllib.request
+
+webUrl = urllib.request.urlopen('https://www.imdb.com/user/ur57221798/ratings/export')
+print(webUrl.getcode()))
