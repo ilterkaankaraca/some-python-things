3960c74b15c360e2f6e02b2e4a4d60beb44189a6
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-07-25 22:46:48 +0200

    Delete imdb.py

--- imdb.py
+++
@@ -1,6 +0,0 @@
-#to get rating information 
-#https://www.imdb.com/user/ur57221798/ratings/export
-import urllib.request
-
-webUrl = urllib.request.urlopen('https://www.imdb.com/user/ur57221798/ratings/export')
-print(webUrl.getcode()))
