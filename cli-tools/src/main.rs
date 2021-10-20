32d381748be4e16d811b601a9959e125d48eff7a
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-10-20 23:21:32 +0200

    Update main.rs

--- cli-tools/src/main.rs
+++ cli-tools/src/main.rs
@@ -1,5 +1,9 @@
-use std::{env, io::{self, Write}, path::Path};
-
+use std::fs::{self, DirEntry};
+use std::{
+    env,
+    io::{self, Write},
+    path::Path,
+};
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1] == "rfe" {
@@ -10,13 +14,28 @@ fn main() {
 }
 
 fn replace_file_extension() {
-    print!("Please provide a path: ");
-    io::stdout().flush().unwrap();
-    let mut path = String::new();
-    std::io::stdin().read_line(&mut path).unwrap();
-    println!("{}", path); 
-   //TODO: check if path is valid
+    let mut file_names: Vec<DirEntry> = Vec::new();
+    let path = std::env::current_dir().unwrap();
+    println!("{}", path.display());
+    println!("{}", Path::new(&path).exists());
+    let mut counter = 0;
+    for entry in fs::read_dir(path).unwrap() {
+        // println!("{}", entry.unwrap().path().display());
+        if entry.as_ref().unwrap().path().is_file() {
+            println!("{}", counter);
+            counter += 1;
+        } else {
+            continue;
+        }
+    }
 }
+
+// onlyfiles = [f for f in listdir() if isfile(join(f))]
+// for i in onlyfiles:
+//     if os.path.splitext(i)[1] != '.py':
+//         base = os.path.splitext(i)[0]
+//         os.rename(i, base + '.sql')
+//TODO: check if path is valid
 
 fn only_keep_selected_file_type() {
     println!("only_keep_selected_file_type");
