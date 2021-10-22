3c0e7dbdefb7ad6a0847f41dde2fe8e1843b32f4
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-10-22 12:03:22 +0200

    Delete replace_file_extension.py

--- replace_file_extension.py
+++
@@ -1,8 +0,0 @@
-import os
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir() if isfile(join(f))]
-for i in onlyfiles:
-    if os.path.splitext(i)[1] != '.py':
-        base = os.path.splitext(i)[0]
-        os.rename(i, base + '.sql')
